19d6d7e989296c702e6c9723861696fda6dcee7f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-06-18 20:07:18 +0100

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UK-Wide SVX Node Monitor</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
     <!-- Header Section -->
